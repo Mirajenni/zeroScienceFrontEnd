5c9576562a97ca41534ca71e26ff299247b1825f
Gabriel Amorim <gabriel.2019206500@unicap.br>
2020-10-03 14:33:27 -0300

    Subindo NAVE

---
+++ zero-science-frontend/src/Login/Nave/Nave.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Nave extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ARUBA</h1>
+      </div>
+    );
+  }
+}
--- zero-science-frontend/src/index.js
+++ zero-science-frontend/src/index.js
@@ -4,12 +4,14 @@ import "./index.scss";
 import App from "./App";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./Login/Login";
+import Nave from "./Login/Nave/Nave";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact={true} component={App} />
       <Route path="/login" component={Login} />
+      <Route path="/nave" component={Nave} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
