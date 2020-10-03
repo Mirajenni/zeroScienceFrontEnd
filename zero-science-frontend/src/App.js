312d9d25bbb141b2eb9f6dd6618c8cc56c6c92aa
mmariabeatriz <m.beatrizcb@hotmail.com>
2020-10-03 14:30:54 -0300

    Tela de login criada

--- zero-science-frontend/src/App.js
+++ zero-science-frontend/src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import "./App.scss";
+import Login from "./Login/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Login/>
+    </section>
   );
 }
 
--- zero-science-frontend/src/Login/Login.jsx
+++ zero-science-frontend/src/Login/Login.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
-
+import "./Login.scss"
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
+      <div className="content">
+        <div className="user-login">
+          <h1 className="user-login__title">Login</h1>
+          <form>
+            <div className="user-login__form-control">
+              <label htmlFor="user">Usuário</label>
+              <input
+                id="user"
+                type="text"
+                name="user"
+              />
+            </div>
+            <div className="user-login__form-control">
+              <label htmlFor="password">Senha</label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+              />
+            </div>
+            <button
+              type="submit"
+              theme="contained-green"
+              className="user-login__submit-button"
+              rounded
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
       </div>
+      
     );
   }
 }
