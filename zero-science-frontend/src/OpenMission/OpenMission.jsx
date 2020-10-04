d7f46e0cb9d1243a33d224d5de880f3f00215d86
mmariabeatriz <m.beatrizcb@hotmail.com>
2020-10-04 17:38:06 -0300

    incluindo terra na mission

--- zero-science-frontend/src/OpenMission/OpenMission.jsx
+++ zero-science-frontend/src/OpenMission/OpenMission.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./OpenMission.scss"
+import earth from "../Assets/earth.svg"
 
 export default class OpenMission extends Component {
   render() {
     return (
         <div className="openmission-page-content">
             <div className="earth">
-
+              <img src={earth} alt=""/>
             </div>
         </div>
       
