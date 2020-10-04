f64d914bb084c3646ecedb6f07e40552c74d2bf0
Gabriel Amorim <gabriel.2019206500@unicap.br>
2020-10-04 19:22:49 -0300

    Adjust margins and Trial

--- zero-science-frontend/src/index.js
+++ zero-science-frontend/src/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Login/Login";
 import MainPage from "./MainPage/MainPage";
 import mundo from "./Mundo/mundo";
 import Planets from "./Planets/Planets";
+import Trial from "./Trial/Trial";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact={true} component={App} />
+      <Route path="/" exact={true} component={Trial} />
       <Route path="/login" component={Login} />
+      <Route path="/trial" component={Trial} />
       <Route path="/home" component={MainPage} />
       <Route path="/mundo" component={mundo} />
       <Route path="/planets" component={Planets} />
