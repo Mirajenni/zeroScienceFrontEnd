1fc9f33a9f7495815660f501e879c9e9ec2f1f1d
mmariabeatriz <m.beatrizcb@hotmail.com>
2020-10-04 13:11:43 -0300

    tela de missao aberta criada && rotas para trial e opan-mission criadas

---
+++ zero-science-frontend/src/OpenMission/OpenMission.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./OpenMission.scss"
+
+export default class OpenMission extends Component {
+  render() {
+    return (
+        <div></div>
+      
+    );
+  }
+}
--- zero-science-frontend/src/index.js
+++ zero-science-frontend/src/index.js
@@ -4,12 +4,16 @@ import "./index.scss";
 import App from "./App";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./Login/Login";
+import Trial from "./Trial/Trial";
+import OpenMission from "./OpenMission/OpenMission";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact={true} component={App} />
       <Route path="/login" component={Login} />
+      <Route path="/trial" component={Trial} />
+      <Route path="/open-mission" component={OpenMission} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
